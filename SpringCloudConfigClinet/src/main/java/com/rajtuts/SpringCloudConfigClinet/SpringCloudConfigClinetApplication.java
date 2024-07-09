78def724392fa32cd0953f1b8b90f71bbe17ff39
rajtuts <rajtuts79@gmail.com>
2024-07-09 00:08:49 -0500

    Add files via upload

---
+++ SpringCloudConfigClinet/src/main/java/com/rajtuts/SpringCloudConfigClinet/SpringCloudConfigClinetApplication.java
@@ -0,0 +1,13 @@
+package com.rajtuts.SpringCloudConfigClinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClinetApplication.class, args);
+	}
+
+}
---
+++ SpringCloudConfigClinet/src/test/java/com/rajtuts/SpringCloudConfigClinet/SpringCloudConfigClinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajtuts.SpringCloudConfigClinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigClinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
